fbe77c47359dc4f4d2e3223990ccdc12c917d989
Timo Haeckel <timo.haeckel@haw-hamburg.de>
2026-02-16 11:32:23 +0100

    more documentation

--- carnet/series_carnet.bash
+++ carnet/series_carnet.bash
@@ -16,21 +16,11 @@ python topo-carnet-hostperservice.py --evaluate F --runs $numruns >> "$logfile" 
 f_time=$(date +%s)
 echo "F evaluation done after $((f_time - h_time)) seconds" >> "$logfile"
 sleep 1
-# echo "Starting E evaluation" >> "$logfile"
-# python topo-carnet-hostperservice.py --evaluate E --runs $numruns >> "$logfile" 2>&1
-# e_time=$(date +%s)
-# echo "E evaluation done after $((e_time - f_time)) seconds" >> "$logfile"
-# sleep 1
 echo "Starting A evaluation" >> "$logfile"
 python topo-carnet-hostperservice.py --evaluate A --runs $numruns >> "$logfile" 2>&1
 a_time=$(date +%s)
 echo "A evaluation done after $((a_time - f_time)) seconds" >> "$logfile"
 sleep 1
-# echo "Starting G evaluation" >> "$logfile"
-# python topo-carnet-hostperservice.py --evaluate G --runs $numruns >> "$logfile" 2>&1
-# g_time=$(date +%s)
-# echo "G evaluation done after $((g_time - a_time)) seconds" >> "$logfile"
-# sleep 1
 
 end_time=$(date +%s)
 total_time=$((end_time - start_time))
